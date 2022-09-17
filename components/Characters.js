5b8cab3c559a77c288ef2fd5e3241eb0bf4158c6
gvirus21 <gvirus21yt@gmail.com>
2022-09-17 11:09:14 +0530

    Implemented pagination

--- components/Characters.js
+++ components/Characters.js
@@ -1,18 +1,20 @@
 import { useQuery } from 'react-query';
 import Character from './Character';
 import styles from '../styles/Character.module.css'
+import { useState } from 'react';
 
 
 const Characters = () => {
+    const [page, setPage] = useState(1)
 
-    
-    const fetchCharacters = async () => {
-        const response = await fetch("https://rickandmortyapi.com/api/character")
+    const fetchCharacters = async ({queryKey}) => {
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${queryKey[1]}`)
         const data = await response.json()
-        return data.results
+        return data
     }
     
-    const { data: characters, status } = useQuery("characters", fetchCharacters)
+    const { data, status } = useQuery(["characters", page], fetchCharacters)
+    console.log(data)
    
     if (status === "loading") {
         return <div>Loading...</div>
@@ -24,10 +26,14 @@ const Characters = () => {
   return (
     <div className={styles.cardContainer}>
         {
-            characters.map((character) => {
+            data.results.map((character) => {
                 return <Character character={character} />
             })
         }
+        <div className={styles.buttonContainer}>
+            <button disabled={page===1} onClick={() => setPage((prev) => (prev - 1))} className={styles.button}>Previous</button>
+            <button disabled={!data.info.next} className={styles.button} onClick={() => setPage((prev) => (prev + 1))}>Next</button>
+        </div>
     </div>
   )
 }
